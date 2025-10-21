0527fe8d104ee01158f368688d425e6cd7664c8a
HJBCodeForge <henning@hjbcodeforge.com>
2025-10-21 09:49:10 -0400

    find the max value method

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
 //---Example of stateful and stateless (static) methods---
 //Stateful methods rely on instance data (object state) to perform their operations, 
 //Stateless(static) methods do not depend on any instance data and can be called without creating an instance of the class.
-Random dice = new(); //stateful method
-int roll = dice.Next(1, 7); //stateless (static) method
-Console.WriteLine(roll);
+// Random dice = new(); //stateful method
+// int roll = dice.Next(1, 7); //stateless (static) method
+// Console.WriteLine(roll);
 
 //---VOID METHODS---, which do not return a value
 //Other methods return a value, the return value is typically the result of an operation. 
@@ -18,9 +18,32 @@ Console.WriteLine(roll);
 //---Overloaded methods---
 //Methods can be overloaded by defining multiple methods with the same name but different parameter lists (different types or number of parameters).
 //The appropriate method is selected based on the arguments provided during the method call.
-int number = 7;
-string text = "seven";
+// int number = 7;
+// string text = "seven";
 
-Console.WriteLine(number); //int as parameter
-Console.WriteLine(); //zero parameters
-Console.WriteLine(text); //string as parameter
+// Console.WriteLine(number); //int as parameter
+// Console.WriteLine(); //zero parameters
+// Console.WriteLine(text); //string as parameter
+
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+// dice.Next(roll3);
+
+//Math class methods challenge
+//Implement a method of the Math class that returns the larger of two numbers
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue;
+
+// largerValue = Math.Max(firstValue, secondValue);
+
+// Console.WriteLine(largerValue);
+
+/*---DECISION MAKING LOGIC---*/
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34d569bca2a71c2135c99e90e866ab8924de3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfee3094a36ec7d2d2e9e5f66be717c34b1d97c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
