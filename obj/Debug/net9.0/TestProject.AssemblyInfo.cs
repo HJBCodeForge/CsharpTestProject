1f68694d99b48662a3d3653c6ffe451ddb98f6f8
HJBCodeForge <henning@hjbcodeforge.com>
2025-10-23 12:56:06 -0400

    added more students

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3387b8c4d0b06a439673e181e468d4202a700ea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54f659a7e8e09d5fa4b2da009139af047a176d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
